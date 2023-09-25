12336af4e1884fa5d1090c472c97027d38e8493d
kenn3y <125267353+kenn3y@users.noreply.github.com>
2023-09-25 18:55:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const buttonTextEl = document.querySelector('.buttonText');
 const inputEl = document.querySelector('.input')
 
 function fetchData(){
-   fetch('https://reqres.in/api/users')
+  
+  fetch('https://reqres.in/api/users')
     .then(res => {
         return res.json();
     })
     .then(data =>  {
-        console.log(data.data)  
+        //console.log(data.data)  
         data.data.forEach(element => { 
             const markup = `<li class="list__item">${element.id} : ${element.first_name} 
               ${element.last_name} - ${element.email}</li>`
@@ -19,7 +20,18 @@ function fetchData(){
       });   
     };   
 
+function fetchData2(){
+//listEl.textContent =''
+//  if (buttonTextEl.textContent='Back')fetchData()
+//console.log('fetch2')
+}
+
 buttonEl.addEventListener('click',function(){
+   if(buttonTextEl.textContent ==='Back') {
+    listEl.innerHTML = " "
+    fetchData()
+    return
+   }
     if (dataViewEl.textContent != "data overview")
     { dataViewEl.textContent = 'data overview' 
     fetchData();
@@ -27,4 +39,14 @@ buttonEl.addEventListener('click',function(){
     } else location.reload();
 })
 
-
+const listClickHandler = () =>
+{
+  event.preventDefault()
+  const clickValue = event.target
+  const myArrayText = clickValue.textContent.split(" ")
+  //console.log('list clicked', clickValue, myArrayText[0])
+  listEl.innerHTML = `<img src="https://reqres.in/img/faces/${myArrayText[0]}-image.jpg"> <li>${myArrayText[19]}</li>`
+  buttonTextEl.innerHTML = 'Back'
+  dataViewEl.textContent = 'Details'
+}
+listEl.addEventListener('click',listClickHandler)
