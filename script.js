1875f2e2e79ba2453da9253bddf3524e7dfa35b2
kenn3y <125267353+kenn3y@users.noreply.github.com>
2023-09-25 13:07:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const dataViewEl = document.querySelector('.dataView');
+const buttonEl =document.querySelector('.counter__reset-button');
+const listEl = document.querySelector('.list');
+const buttonTextEl = document.querySelector('.buttonText');
+const inputEl = document.querySelector('.input')
+
+function fetchData(){
+   fetch('https://reqres.in/api/users')
+    .then(res => {
+        return res.json();
+    })
+    .then(data =>  {
+        console.log(data.data)  
+        data.data.forEach(element => { 
+            const markup = `<li>${element.id} : ${element.first_name} 
+              ${element.last_name} - ${element.email}</li>`
+            listEl.insertAdjacentHTML('beforeend',markup)
+        });
+      });   
+    };   
+
+buttonEl.addEventListener('click',function(){
+    if (dataViewEl.textContent != "data overview")
+    { dataViewEl.textContent = 'data overview' 
+    fetchData();
+    buttonTextEl.innerHTML = 'Reset'
+    } else location.reload();
+})
+
+
